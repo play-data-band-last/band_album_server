de4d1d1851678ef14110cec355fff05378d019c5
dokyeong99 <dlehrud451@gmail.com>
2023-10-11 19:25:08 +0900

    a

--- src/main/java/com/example/album/kafka/AlbumConsumer.java
+++ src/main/java/com/example/album/kafka/AlbumConsumer.java
@@ -5,22 +5,32 @@ import com.example.album.domain.request.LikeCountUpdateRequest;
 import com.example.album.domain.request.UserUpdateRequest;
 import com.example.album.service.AlbumService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AlbumConsumer {
     private final AlbumService albumService;
-
+    @RetryableTopic
     @KafkaListener(topics = TopicConfig.album)
     public void listen(LikeCountUpdateRequest likeCountUpdateRequest) {
         albumService.likeCountUpdate(likeCountUpdateRequest);
     }
 
-    @KafkaListener(topics = TopicConfig.albumUpdate)
+    @RetryableTopic
+    @KafkaListener(topics = TopicConfig.memberUpdate)
     public void updateMember(UserUpdateRequest userUpdateRequest) {
         albumService.memberUpdateInAlbum(userUpdateRequest);
     }
+
+    @DltHandler
+    public void processDltMessage(String dltMessage) {
+        // DLT 토픽에서 메시지를 처리합니다. (예: 로깅 또는 추가 조사)
+        System.out.println("DLT에서 메시지 수신: " + dltMessage);
+    }
+
 
 }
--- src/main/java/com/example/album/kafka/TopicConfig.java
+++ src/main/java/com/example/album/kafka/TopicConfig.java
@@ -1,11 +1,28 @@
 package com.example.album.kafka;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TopicConfig {
     public final static String album = "album";
-    public final static String albumUpdate = "albumUpdate";
+    public static final String albumDlt = "albumDLT";
+
+
+    public final static String memberUpdate = "memberUpdate";
+
+
+    @Bean
+    public NewTopic albumDltTopic() {
+        return TopicBuilder.name(albumDlt)
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
+
+
 
 
 }
